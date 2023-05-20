24f61e705cf0dd94f14aa6ce80353f7e741a705b
katya sitko <kat.sitko@gmail.com>
2023-05-20 12:34:16 +0200

    added Recipe Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_100323) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_103223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "first_name"
+    t.string "string"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "food_categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_100323) do
     t.index ["food_category_id"], name: "index_food_items_on_food_category_id"
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.integer "portion_size"
+    t.bigint "chef_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chef_id"], name: "index_recipes_on_chef_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +59,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_100323) do
   end
 
   add_foreign_key "food_items", "food_categories"
+  add_foreign_key "recipes", "chefs"
 end
