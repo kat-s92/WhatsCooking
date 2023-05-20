4d374dda65ac88ffed1a2d93d42a35c5e1663b1c
katya sitko <kat.sitko@gmail.com>
2023-05-20 16:20:22 +0200

    added the SavedChefController

--- app/controllers/saved_chefs_controller.rb
+++ app/controllers/saved_chefs_controller.rb
@@ -1,2 +1,26 @@
 class SavedChefsController < ApplicationController
+  def index
+    @saved_chefs = SavedChefs.all
+  end
+
+  def new
+    @saved_chef = SavedChef.new
+  end
+
+  def create
+    @saved_chef = SavedChef.new(saved_chef_params)
+    @saved_chef.user = current.user
+    @saved_chef.chef = Chef.find(params[:id])
+  end
+
+  # def delete
+  #   @saved_chef = SavedChef.find(params[id])
+  #   @saved_chef.destroy
+  # end
+
+  private
+
+  def saved_chef_params
+    params.require(saved_chef).permit(:user_id, :chef_id)
+  end
 end
