20bf8c4f69da2955eb887c73b00a6d75f897bbcf
Aishwarya Kanitkar <aishwaryakanitkar25@gmail.com>
2023-05-20 15:27:08 +0200

    Added the routes to the app

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # We do not have a user dashboard because the saved_recipes and saved_chefs dashboard would be the index for saved_recipes and saved_chefs (similar to booking dashboard in rent_my_shoes)
+  resources :food_categories, only: %i[index] do
+    resources :food_items, only: %i[index]
+  end
+  resources :recipes, only: %i[index show] do
+    resources :reviews, only: %i[index show create new]
+  end
+
+  resources :shopping_carts, only: %i[show]
+  resources :saved_chefs, only: %i[index create new destroy]
+  resources :saved_recipes, only: %i[index create new destroy]
 end
