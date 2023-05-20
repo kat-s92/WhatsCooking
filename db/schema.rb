6282dba7b2783ec84d9a51404d19977f5a836c8a
Aishwarya Kanitkar <aishwaryakanitkar25@gmail.com>
2023-05-20 12:25:46 +0200

    Created the shooping_cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_100323) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_102106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_100323) do
     t.index ["food_category_id"], name: "index_food_items_on_food_category_id"
   end
 
+  create_table "shopping_carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +48,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_100323) do
   end
 
   add_foreign_key "food_items", "food_categories"
+  add_foreign_key "shopping_carts", "users"
 end
