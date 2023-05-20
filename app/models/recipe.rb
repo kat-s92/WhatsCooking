813dfe264570fec71b9688d11956f1c4b310ac07
Giulio Mauriello <giuliom.90@live.it>
2023-05-20 14:54:57 +0200

    created saved recipes model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :chef
+  has_many :saved_recipes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_105707) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_123804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_105707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_category_id"], name: "index_food_items_on_food_category_id"
+  end
+
+  create_table "missing_items", force: :cascade do |t|
+    t.bigint "food_item_id", null: false
+    t.bigint "shopping_cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_item_id"], name: "index_missing_items_on_food_item_id"
+    t.index ["shopping_cart_id"], name: "index_missing_items_on_shopping_cart_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -70,6 +79,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_105707) do
   end
 
   add_foreign_key "food_items", "food_categories"
+  add_foreign_key "missing_items", "food_items"
+  add_foreign_key "missing_items", "shopping_carts"
   add_foreign_key "recipes", "chefs"
   add_foreign_key "shopping_carts", "users"
   add_foreign_key "users", "shopping_carts"
