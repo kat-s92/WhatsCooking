5d1049b8c761fbc0f7d4f96da25e6ec3fb342932
Giulio Mauriello <giuliom.90@live.it>
2023-05-20 16:48:31 +0200

    reviews and saved recipes controllers created

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.recipe_id = Review.find(params[:recipe_id])
+    @review.user_id = current_user.id
+    @review.save
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+end
