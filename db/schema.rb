50f081392ddedc570c08349fa5b44cdfb2d5e5e2
katya sitko <kat.sitko@gmail.com>
2023-05-20 12:46:23 +0200

    added Recipe Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_20_102106) do
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_103223) do
+# ActiveRecord::Schema[7.0].define(version: 2023_05_20_103223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
