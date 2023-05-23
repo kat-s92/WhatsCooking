40347b9a4a485134ee753c05134e2415562aee86
Aishwarya Kanitkar <aishwaryakanitkar25@gmail.com>
2023-05-23 21:26:50 +0200

    Added the checkboxes for the food items

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -1,5 +1,6 @@
 class FoodItemsController < ApplicationController
   def index
-    @food_items = FoodItem.all
+    @food_items = FoodItem.where(food_category_id: params[:food_category_id])
+    @categories = ["Ruby", "JavaScript", "CSS"]
   end
 end
