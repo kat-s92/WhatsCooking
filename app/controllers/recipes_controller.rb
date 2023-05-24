a04c6abbb8f36beb18c904f26ff4299a58a68a75
katya sitko <kat.sitko@gmail.com>
2023-05-24 08:12:18 +0200

    adding steps to the recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,21 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
+    # @ratings = []
+
+    # @recipes.each do |recipe|
+    #   reviews = recipe.reviews
+    #   reviews.each do |review|
+    #   @ratings << review.rating
+    #   end
+    # end
+
   end
 
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+
+
 end
