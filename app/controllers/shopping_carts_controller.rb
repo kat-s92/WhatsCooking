6e138f6aac1ea0e8449b28c04d41a564c92ae253
katya sitko <kat.sitko@gmail.com>
2023-05-27 12:46:09 +0200

    added rating if else

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,11 +1,12 @@
 class ShoppingCartsController < ApplicationController
-  def index
-  end
+    def index
+    end
 
   def show
     # @shopping_cart = current_user.Shopping_cart
-    @tomato = Food_Item.find_by(name: "tomato")
-    @missing_item = Missing_item.create(food_item_id: @tomato.id)
-    @shopping_cart.new
+    @tomato = FoodItem.find_by(name: "tomato")
+    @missing_item = MissingItem.create(food_item: @tomato)
+    @shopping_cart = ShoppingCart.new(user: current_user, missing_item_id: @missing_item)
+    @shopping_cart.save
   end
 end
