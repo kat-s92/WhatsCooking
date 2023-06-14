0268e70e075301b5286e86200dbbfaf972024bda
katya sitko <kat.sitko@gmail.com>
2023-06-14 16:06:42 +0200

    styling, seeded 1 more recipe, added ingr to recipe steps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_172957) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_115631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_172957) do
     t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ingredients", default: [], array: true
     t.index ["recipe_id"], name: "index_recipe_steps_on_recipe_id"
   end
 
