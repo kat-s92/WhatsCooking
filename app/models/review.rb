bafae077cb821bc20b0c3e318989fe1f2d9e1732
Giulio Mauriello <giuliom.90@live.it>
2023-06-15 19:30:32 +0200

    working on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
+
+
+  def review_user_name
+    current_user.first_name == User.find(user_id).first_name ? current_user.first_name : User.find(user_id).first_name
+  end
+
 end
